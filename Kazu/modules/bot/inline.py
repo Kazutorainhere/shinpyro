3bc2a988d3916bbb7435346ab4d615b35ca056e7
Kazutorainhere <123609176+Kazutorainhere@users.noreply.github.com>
2023-02-07 19:43:46 +0700

    update inline.py

--- Kazu/modules/bot/inline.py
+++ Kazu/modules/bot/inline.py
@@ -84,7 +84,7 @@ async def ping_function(message: Message, answers):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     msg = (
-        f"<b>❃ 𝙿𝚈𝚁𝙾𝚉𝚄-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ❃</b>\n\n"
+        f"<b>𝐒𝐡𝐢𝐧𝐏𝐲𝐫𝐨</b>\n\n"
         f"❃ Pɪɴɢ : `{duration}` ms\n"
         f"❃ Uᴘᴛɪᴍᴇ : "
         f"`{uptime}` \n"
