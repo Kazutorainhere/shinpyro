6a0e4c8a4a7e42be3bac8bbe0dd86ccfc4867eb8
Kazutorainhere <123609176+Kazutorainhere@users.noreply.github.com>
2023-02-07 20:21:41 +0700

    update alive.py

--- Kazu/modules/alive.py
+++ Kazu/modules/alive.py
@@ -35,7 +35,7 @@ modules = CMD_HELP
 alive_logo = (
     gvarstatus("ALIVE_LOGO") or "https:"
 )
-emoji = gvarstatus("ALIVE_EMOJI") or "『★』"
+emoji = gvarstatus("ALIVE_EMOJI") or "★"
 alive_text = gvarstatus("ALIVE_TEKS_CUSTOM") or "✨ᴘʀᴇᴍɪᴜᴍ✨"
 
 
@@ -46,7 +46,7 @@ async def alive(client: Client, message: Message):
     send = client.send_video if alive_logo.endswith(".mp4") else client.send_photo
     uptime = await get_readable_time((time.time() - StartTime))
     man = (
-        f"**༺ [𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://github.com/Kazutorainhere/shinpyro) ༻**\n\n"
+        f"**༺ [𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://github.com/Kazutorainhere/shinpyro ༻**\n\n"
         f"<b>{alive_text}</b>\n\n"
         f"{emoji} <b>Mᴀsᴛᴇʀ :</b> {client.me.mention} \n"
         f"{emoji} <b>Mᴏᴅᴜʟᴇs :</b> <code>{len(modules)} Modules</code> \n"
